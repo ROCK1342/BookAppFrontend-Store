3abfc731489a1a587df72c720de2ca7c48932b2d
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-02-03 21:36:17 +0000

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sholly333/hotel-app:latest
+docker pull sholly333/BookApp-Store:latest
 
 # Run the Docker image as a container
-docker run -dit -p 8084:80 sholly333/hotel-app
+docker run -dit -p 8080:80 sholly333/BookApp-Store
