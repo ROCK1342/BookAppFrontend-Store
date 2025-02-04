9ebdccc6ffe759d8968427fda528a58e60402796
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-02-04 09:27:17 +0000

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 				<div className='brand-and-toggler flex flex-sb'>
 					<Link to='/' className='navbar-brand flex'>
 						<img src={logoImg} alt='site logo' />
-						<span className='text-uppercase fw-7 fs-24 ls-1'>Sholasshub</span>
+						<span className='text-uppercase fw-7 fs-24 ls-1'>Sholashub</span>
 					</Link>
 					<button
 						type='button'
